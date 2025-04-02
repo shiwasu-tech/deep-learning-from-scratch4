82df5e5caf270fa04c31b60358c0860d16a3ff1f
shiwasu-tech <njayamena@gmail.com>
2025-04-02 16:55:47 +0900

    ch06 done

--- main3.py
+++ main3.py
@@ -220,3 +220,98 @@ for episode in range(episodes):
         state = next_state
 env.render_q(agent.Q)
 # %%
+
+from collections import defaultdict
+import numpy as np
+from common.utils import greedy_probs
+
+class QLearningAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.alpha = 0.1
+        self.epsilon = 0.1
+        self.action_size = 4
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.b = defaultdict(lambda: random_actions)
+        self.Q = defaultdict(lambda: 0)
+
+    def get_action(self, state):
+        action_probs = self.b[state]
+        actions = list(action_probs.keys())
+        probs = list(action_probs.values())
+        return np.random.choice(actions, p=probs)
+    
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q_max = 0
+        else:
+            next_qs = [self.Q[next_state, a] for a in range(self.action_size)]
+            next_q_max = max(next_qs)
+        
+        target = reward + self.gamma * next_q_max
+        self.Q[state, action] += self.alpha * (target - self.Q[state, action])
+
+        self.pi[state] = greedy_probs(self.Q, state, epsilon=0)
+        self.b[state] = greedy_probs(self.Q, state, self.epsilon)
+
+# %%
+
+class QLearningAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.alpha = 0.8
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.b = defaultdict(lambda: random_actions)
+        self.Q = defaultdict(lambda: 0)
+    
+    def get_action(self, state):
+        self.b[state] = greedy_probs(self.Q, state, self.epsilon)
+
+        action_probs = self.b[state]
+        actions = list(action_probs.keys())
+        probs = list(action_probs.values())
+        return np.random.hoice(actions, p=probs)
+    
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q_max = 0
+        else:
+            next_qs = [self.Q[next_state, a] for a in range(self.action_size)]
+            next_q_max = max(next_qs)
+
+        target = reward + self.gamma * next_q_max
+        self.Q[state, action] += self.alpha * (target - self.Q[state, action])
+
+# %%
+class QLearningAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.alpha = 0.8
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        self.Q = defaultdict(lambda: 0)
+    
+    def get_action(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.action_size)
+        else:
+            qs = [self.Q[state, a] for a in range(self.action_size)]
+            return np.argmax(qs)
+    
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q_max = 0
+        else:
+            next_qs = [self.Q[next_state, a] for a in range(self.action_size)]
+            next_q_max = max(next_qs)
+
+        target = reward + self.gamma * next_q_max
+        self.Q[state, action] += self.alpha * (target - self.Q[state, action])
+
+
+# %%
