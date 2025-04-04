9d3a0fd6d3f4007fe98392f873ac91448abf89a5
shiwasu-tech <njayamena@gmail.com>
2025-04-04 11:41:34 +0900

    ch07 done

--- main5.py
+++ main5.py
@@ -40,7 +40,7 @@ print(Q[state, action])
 class QNet(nn.Module):
     def __init__(self):
         super(QNet,self).__init__()
-        self.l1 = nn.Linear(1 ,100)
+        self.l1 = nn.Linear(12 ,100)
         self.l2 = nn.Linear(100, 4)
 
     def forward(self, x):
@@ -75,10 +75,11 @@ class QLearningAgent:
             next_q = torch.tensor([0.0])
         else:
             next_qs = self.qnet(next_state)
-            next_q = torch.max(next_qs, dim=1).detach()
+            next_q = torch.max(next_qs, dim=1)[0].detach()
         
         target = reward + self.gamma * next_q
-
+        qs = self.qnet(state)
+        q_value = qs[0, action]
         loss = F.mse_loss(q_value, target)
 
         self.optimizer.zero_grad()
@@ -86,4 +87,42 @@ class QLearningAgent:
         self.optimizer.step()
         return loss.item()
 
+# %%
+from common.gridworld import GridWorld
+
+env = GridWorld()
+agent = QLearningAgent()
+
+episodes = 1000
+loss_history = []
+
+for episode in range(episodes):
+    state = env.reset()
+    state = one_hot(state)
+    total_loss, cnt = 0, 0
+    done = False
+
+    while not done:
+        action = agent.get_action(state)
+        next_state, reward, done = env.step(action)
+        next_state = one_hot(next_state)
+
+        loss = agent.update(state, action, reward, next_state, done)
+        total_loss += loss
+        cnt += 1
+        state = next_state
+    
+    average_loss = total_loss / cnt
+    loss_history.append(average_loss)
+
+# %%
+import matplotlib.pyplot as plt
+plt.plot(loss_history)
+# %%
+Q = {}
+for state in env.states():
+    for action in env.action_space:
+        q = agent.qnet(one_hot(state))[:, action]
+        Q[state, action] = float(q.data)
+env.render_q(Q)
 # %%
